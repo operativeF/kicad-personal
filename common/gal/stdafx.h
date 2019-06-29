153cad2f1e1f94d2dbee32f0836606df096327e5
operativeF <splines21@outlook.com>
2019-06-28 21:27:44 -0500

    Cotire testing part 1
    (cherry picked from commit e5bd299c2454457d61932ce29d2e796e595e4e86)

---
+++ common/gal/stdafx.h
@@ -0,0 +1,15 @@
+// stdafx.h
+
+#include <GL/glew.h>
+
+#include <algorithm>
+#include <cassert>
+#include <cstring>
+#include <vector>
+
+#include <gal/opengl/utils.h>
+#include <gal/opengl/vertex_item.h>
+#include <gal/opengl/vertex_manager.h>
+#include <gal/stroke_font.h>
+
+#include <wx/log.h>
---
+++ pagelayout_editor/stdafx.h
@@ -0,0 +1,10 @@
+// stdafx.h
+
+#include <pl_editor_frame.h>
+#include <tools/pl_actions.h>
+#include <tools/pl_drawing_tools.h>
+#include <tools/pl_selection_tool.h>
+
+#include <ws_draw_item.h>
+#include <ws_data_item.h>
+#include <ws_data_model.h>
